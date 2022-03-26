6cc34a8a4024a278fc67d3a36cb615bcbed344b5
Henrique Andrade <henrique.ccda@gmail.com>
2022-03-26 16:40:02 -0300

    Fim da secção 3.

--- typeScriptParte1/dist/js/app.js
+++ typeScriptParte1/dist/js/app.js
@@ -1,10 +1,7 @@
 import { NegocicaoController } from "./controllers/negociacao-controlles.js";
-
 const controller = new NegocicaoController();
-
 const form = document.querySelector(".form");
-
 form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
-})
+});
